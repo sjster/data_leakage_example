ef317379d9947c2b5f2dcc6cb4e8d9579c37767d
Srijith Rajamohan <sjster@gmail.com>
2022-03-11 22:06:19 -0500

    Edits

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.datasets import load_diabetes, load_iris, load_boston, fetch_california_housing
 
-PREPROCESS_BEFORE_SPLIT = False
+PREPROCESS_BEFORE_SPLIT = True
 
 def train_and_evaluate(fn, X_data_train, X_data_test, y_data_train, y_data_test):
     fn.fit(X_data_train, y_data_train)
@@ -18,7 +18,7 @@ def train_and_evaluate(fn, X_data_train, X_data_test, y_data_train, y_data_test)
     print('R2 score is ',r2_scr)
     print('Mean absolute error is ',mas)
     print('Mean squared error is ',mse)
-    return(r2_score, mas, mse)
+    return(r2_scr, mas, mse)
 
 def get_diabetes_data():
     diabetes = load_diabetes()
@@ -85,13 +85,29 @@ X, y = get_boston_data()
 std_arrays_boston =  run_pipeline(X, y, standardize_data, 'BOSTON')
 norm_arrays_boston = run_pipeline(X, y, normalize_data, 'BOSTON')
 none_arrays_boston = run_pipeline(X, y, None, 'BOSTON')
+df_boston = pd.DataFrame({'std_boston_r2': std_arrays_boston[0],
+        'std_boston_mae': std_arrays_boston[1], 'std_boston_mse': std_arrays_boston[2],
+        'norm_boston_r2': norm_arrays_boston[0], 'norm_boston_mae': norm_arrays_boston[1], 'norm_boston_mse': norm_arrays_boston[2],
+        'none_boston_r2': none_arrays_boston[0], 'none_boston_mae': none_arrays_boston[1], 'none_boston_mse': none_arrays_boston[2]})
 
 X, y = get_diabetes_data()
 std_arrays_diabetes = run_pipeline(X, y, standardize_data, 'DIABETES')
 norm_arrays_diabetes = run_pipeline(X, y, normalize_data, 'DIABETES')
 none_arrays_diabetes = run_pipeline(X, y, None, 'DIABETES')
+df_diabetes = pd.DataFrame({'std_diabetes_r2': std_arrays_diabetes[0],
+        'std_diabetes_mae': std_arrays_diabetes[1], 'std_diabetes_mse': std_arrays_diabetes[2],
+        'norm_diabetes_r2': norm_arrays_diabetes[0], 'norm_diabetes_mae': norm_arrays_diabetes[1], 'norm_diabetes_mse': norm_arrays_diabetes[2],
+        'none_diabetes_r2': none_arrays_diabetes[0], 'none_diabetes_mae': none_arrays_diabetes[1], 'none_diabetes_mse': none_arrays_diabetes[2]})
 
 X, y = get_california_data()
 std_arrays_california = run_pipeline(X, y, standardize_data, 'CALIFORNIA')
 norm_arrays_california =run_pipeline(X, y, normalize_data, 'CALIFORNIA')
 none_arrays_california = run_pipeline(X, y, None, 'CALIFORNIA')
+df_california = pd.DataFrame({'std_california_r2': std_arrays_california[0],
+        'std_california_mae': std_arrays_california[1], 'std_california_mse': std_arrays_california[2],
+        'norm_california_r2': norm_arrays_california[0], 'norm_california_mae': norm_arrays_california[1], 'norm_california_mse': norm_arrays_california[2],
+        'none_california_r2': none_arrays_california[0], 'none_california_mae': none_arrays_california[1], 'none_california_mse': none_arrays_california[2]})
+
+df_boston.to_csv(f'df_boston_{PREPROCESS_BEFORE_SPLIT}.csv')
+df_diabetes.to_csv(f'df_diabetes_{PREPROCESS_BEFORE_SPLIT}.csv')
+df_california.to_csv(f'df_california_{PREPROCESS_BEFORE_SPLIT}.csv')
